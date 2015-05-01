4d3d84987e9060d3595a8f7abc2853c1b8351571
seanlyons <seanlyons@gmail.com>
2015-05-01 04:32:35 -0700

    completed 1-3, starting on 4.
    next:
    http://unity3d.com/learn/tutorials/projects/roll-a-ball/moving-the-camera

---
+++ Assets/scripts/cameracontroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameracontroller : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after all other transforms have run.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
